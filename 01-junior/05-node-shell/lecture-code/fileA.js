a1ee08810f457413fa9a5c319a6b66436f50b553
Kate Humphrey <kate@fullstackacademy.com>
2017-11-03 15:25:01 -0400

    live codee example

--- 01-junior/05-node-shell/lecture-code/fileA.js
+++ 01-junior/05-node-shell/lecture-code/fileA.js
@@ -1,2 +1,26 @@
+const password = 909090
+
+
 console.log('File A')
-console.log(window)
+const kate = 'kate'
+
+function thing () {
+	var x = {}
+	x.exports = 5
+	x.exports = 8
+	console.log('X ', x.exports)
+}
+module.exports = { 
+	password,
+	kate,
+	thing
+};
+
+// module.exports.password = password;
+// module.exports.kate = 'kate'
+
+// to get file B to run we have to require it here
+// require('./fileB.js')
+
+// console.log(document.body)
+// console.dir(document.body)
--- 01-junior/05-node-shell/lecture-code/fileB.js
+++ 01-junior/05-node-shell/lecture-code/fileB.js
@@ -1,1 +1,14 @@
-console.log('File B')
+// const thingExportedFromFileA = require('./fileA')
+const fs = require('fs');
+const express = require('express')
+
+console.log('EXPRESS: ', express);
+
+// console.log('File: ',fs.readFileSync('./README.md', 'utf-8'))
+
+// require('./fileA') // singleton! FileA doesn't log twice
+
+// console.log(thingExportedFromFileA)
+// console.log("password ", thingExportedFromFileA.password)
+// thingExportedFromFileA.thing()
+
