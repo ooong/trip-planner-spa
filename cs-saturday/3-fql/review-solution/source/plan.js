71e2e034dfa44dac09de46a3226a6fcbdf20be0b
Omri Bernstein <hippiccolo@gmail.com>
2017-11-18 16:14:21 -0500

    fql: add review solution for parts I and II

--- cs-saturday/3-fql/review-solution/source/plan.js
+++ cs-saturday/3-fql/review-solution/source/plan.js
@@ -1,3 +1,45 @@
-function Plan () {}
+class Plan {
+  copy () {
+    const copiedPlan = new Plan();
+    return Object.assign(copiedPlan, this);
+  }
+  setLimit (amount) {
+    this._limit = amount;
+    return this;
+  }
+  withinLimit (rows) {
+    if (!this.hasOwnProperty('_limit')) return true;
+    return rows.length < this._limit;
+  }
+  setSelected (columns) {
+    if (columns.includes('*')) delete this._selected;
+    else this._selected = columns;
+    return this;
+  }
+  selectColumns (row) {
+    if (!this.hasOwnProperty('_selected')) return row;
+    const selectedRow = {};
+    for (const column of this._selected) {
+      selectedRow[column] = row[column];
+    }
+    return selectedRow;
+  }
+  setCriteria (criteria) {
+    this._criteria = criteria;
+    return this;
+  }
+  matchesRow (row) {
+    if (!this.hasOwnProperty('_criteria')) return true;
+    return Object.keys(this._criteria).every((column) => {
+      const val = row[column];
+      const cond = this._criteria[column];
+      if (typeof cond === 'function') {
+        return cond(val);
+      } else {
+        return cond === val;
+      }
+    });
+  }
+}
 
 module.exports = Plan;
--- cs-saturday/3-fql/review-solution/source/table.js
+++ cs-saturday/3-fql/review-solution/source/table.js
@@ -1,5 +1,30 @@
 const fs = require('fs');
+const path = require('path');
 
-function Table () {}
+// class syntax...
+class Table {
+  constructor (folderPath) {
+    this._folderPath = folderPath;
+  }
+  read (id) {
+    const filename = Table.toFilename(id);
+    const filepath = path.join(this._folderPath, filename);
+    if (!fs.existsSync(filepath)) return undefined;
+    const contents = fs.readFileSync(filepath);
+    const row = JSON.parse(contents);
+    return row;
+  }
+  getRowIds () {
+    const filenames = fs.readdirSync(this._folderPath);
+    const ids = filenames.map(Table.toId);
+    return ids;
+  }
+  static toFilename (id) {
+    return id + '.json';
+  }
+  static toId (filename) {
+    return filename.slice(0, -5);
+  }
+}
 
 module.exports = Table;
